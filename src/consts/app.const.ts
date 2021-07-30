84dfe19246eee23cf46bf4841c36eb2bb0a43f68
le.trong <le.trong>
2021-07-30 03:24:06 -0400

    Updated app consts

--- src/consts/app.const.ts
+++ src/consts/app.const.ts
@@ -1,7 +1,7 @@
 export const AppConsts = {
   ASSETS_COLLECTION: 'assets',
-  ASSET_PRICES_COLLECTION: 'asset_prices',
-  ASSET_SECTORS_COLLECTION: 'asset_sectors',
-  ASSET_COUNTRIES_COLLECTION: 'asset_countries',
-  ASSET_DIVIDENDS_COLLECTION: 'asset_dividends'
+  ASSET_PRICES_COLLECTION: 'assetPrices',
+  ASSET_SECTORS_COLLECTION: 'assetSectors',
+  ASSET_COUNTRIES_COLLECTION: 'assetCountries',
+  ASSET_DIVIDENDS_COLLECTION: 'assetDividends'
 };
